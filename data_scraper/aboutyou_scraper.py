793a4c41aa9b1081fd9dbc4ea2b8b3b336752c85
Sona Pecenakova <sona.pecenakova@gmail.com>
2018-02-28 13:21:57 +0100

    fix scraping logic and change path structure

--- data_scraper/aboutyou_scraper.py
+++ data_scraper/aboutyou_scraper.py
@@ -30,13 +30,13 @@ class Scraper():
               'beige': 38919
               }
 
-    CATEGORIES = ['Kleider']
+    CATEGORIES = ['kleider', 'pullover']
     IMAGE_FORMAT = '.jpg'
 
     def __init__(self,
                  data_path=DATA_PATH,
                  chromedriver_path=CHROMEDRIVER_PATH,
-                 color_names=COLORS.keys(),
+                 color_names=list(COLORS.keys()),
                  categories=CATEGORIES,
                  img_format=IMAGE_FORMAT):
 
@@ -58,24 +58,24 @@ class Scraper():
     def download_category(self, category):
 
         category_data_path = os.path.join(self.data_path, category)
-        if not os.path.exists(category_data_path):
-            os.makedirs(category_data_path)
 
-        category_url = self.URL_CLOTHES + '/' + category
-
-        print('\nDownloading category: {} to {}'.format(category, category_data_path))
+        print('\nDownloading category: {}'.format(category))
         print(50 * '-')
 
         subcategories = self.get_subcategories(category)
 
-        for subcat_idx, subcategory in enumerate(subcategories):
+        for subcategory, subcategory_link in subcategories.items():
 
-            print('Downloading sub-category [{:02}/{:02}]: {}'.format(subcat_idx, len(subcategories), subcategory))
-            subcategory_link = category_url + '/' + subcategory
+            print('Downloading sub-category: {}'.format(subcategory))
+
+            subcategory_data_path = os.path.join(category_data_path, subcategory)
+            if not os.path.exists(subcategory_data_path):
+                os.makedirs(subcategory_data_path)
 
             for color_name, color_code in self.colors.items():
 
                 subcategory_color_link = subcategory_link + '?bi_color={}'.format(color_code)
+
                 products = self.download_subcategory_products(subcategory_color_link)
                 print('Color {}: {} products'.format(color_name, len(products)))
 
@@ -83,7 +83,7 @@ class Scraper():
                     name, img_id, img_link, details = self.get_product_info(product)
 
                     # save product image
-                    img_filepath = os.path.join(category_data_path, img_id + self.image_format)
+                    img_filepath = os.path.join(subcategory_data_path, img_id + self.image_format)
                     self.save_product_image(img_link, img_filepath, width=400)
 
                     # save product to dataframe
@@ -93,14 +93,14 @@ class Scraper():
                                     'subcategory': subcategory,
                                     'color': color_name,
                                     'product_name': name,
-                                    'attributes': details}
+                                    'attributes': ','.join(details)}
 
                     self.df_product_info = self.df_product_info.append(product_dict, ignore_index=True)
 
             # save csv after each subcategory
             self.df_product_info = self.df_product_info[['img_path', 'img_url', 'category', 'subcategory', 'color', 'product_name', 'attributes']]
             csv_file = os.path.join(self.data_path, category + '.csv')
-            self.df_product_info.to_csv(csv_file, index=False)
+            self.df_product_info.to_csv(csv_file, index=False, encoding='latin1', sep=';')
             print('Writing sub-category data to: {}'.format(csv_file))
 
 
@@ -113,17 +113,17 @@ class Scraper():
         # get product details
         product_name = product_soup.find('h1', class_='productName_192josg').text
         product_details = product_soup.find('div', class_='wrapper_1w5lv0w')
-        tags = []
+        product_attributes = []
         for detail_section in product_details.find_all('div', class_='container_iv4rb4'):
             for tag in detail_section.find_all('li'):
-                tags.append(tag.text.strip())
+                product_attributes.append(tag.text.strip())
 
         # get product image
         product_img = product.find('div', class_='img_162hfdi-o_O-imgTrimmed_16d3go2')['style']
         product_img_link = 'https:' + product_img.split('"')[1].split('?')[0]
         product_img_id = product_img_link.split('/')[-1]
 
-        return product_name, product_img_id, product_img_link, product_details
+        return product_name, product_img_id, product_img_link, product_attributes
 
     def save_product_image(self, img_link, img_filepath, width=400):
 
@@ -161,10 +161,12 @@ class Scraper():
         category_soup = BeautifulSoup(response.content, 'html.parser')
 
         # find sub-categories of category
-        category_list = category_soup.find('li', title=category).parent
-        sub_categories = []
+        category_list = category_soup.find('li', title=category.title()).parent
+        sub_categories = {}
         for sub_category in category_list.find_all('a'):
-            sub_categories.append(sub_category.text)
+            name = sub_category.text.lower()
+            link = self.URL + sub_category['href']
+            sub_categories[name] = link
 
         print('Found {} subcategories for {}'.format(len(sub_categories), category))
         return sub_categories
@@ -182,7 +184,7 @@ class Scraper():
 
 
 def main():
-    scraper = Scraper()
+    scraper = Scraper(color_names=['yellow'])
     scraper.download_data()
 
 
