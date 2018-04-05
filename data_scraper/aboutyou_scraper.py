f0abc3aedbb80fe4ead125681ff081a1173b30e5
Sona Pecenakova <sona.pecenakova@gmail.com>
2018-04-05 13:27:08 +0200

    remove subcategories

--- data_scraper/aboutyou_scraper.py
+++ data_scraper/aboutyou_scraper.py
@@ -36,8 +36,8 @@ class AboutYouScraper():
               }
 
     # list of clothes categories on the aboutyou website
-    CATEGORIES = ['kleider', 'strick', 'jeans', 'jacken', 'blusen & tuniken',
-                  'röcke', 'shirts', 'hosen', 'jumpsuits & overalls', 'tops']
+    CATEGORIES = ['kleider', 'strick', 'jeans', 'jacken', 'blusen-und-tuniken',
+                  'roecke', 'shirts', 'hosen', 'jumpsuits-und-overalls', 'tops']
 
     IMAGE_FORMAT = '.jpg'
 
@@ -56,22 +56,25 @@ class AboutYouScraper():
         """
 
         self.data_path = data_path
+        if not os.path.exists(data_path):
+            os.makedirs(data_path)
+
+        self.data_csv = os.path.join(self.data_path, 'data.csv')
+
         self.chromedriver_path = chromedriver_path
 
         self.colors = {color_name: self.COLORS[color_name] for color_name in color_names}
         self.categories = categories
 
         # dataframe to hold all images information
-        self.df_product_info = pd.DataFrame(columns=['img_path', 'img_url', 'category', 'subcategory',
-                                                     'color', 'product_name', 'attributes'])
         self.image_format = img_format
 
     def download_data(self):
         """
         Download all data and save the description in data.csv. The flow is the following:
-        -> for each category: get all subcategories links from the menu
-            -> for each subcategory:
-                -> for each color: download all products and save their images and descriptions
+        -> for each category:
+            -> for each color: get number of pages
+                ->for each page: download all products and save their images and descriptions
         """
 
         for category in self.categories:
@@ -80,92 +83,82 @@ class AboutYouScraper():
             except Exception as e:
                 print('Problem with download of category: {}'.format(category), e)
 
-        # save all data csv
-        csv_file = self.data_path + 'data.csv'
-        self.append_csv_file(csv_file, self.df_product_info)
 
     def download_category(self, category):
-        """
-        Download all category's data. Write it to a {category}.csv.
-        """
 
         print('\nDownloading category: {}'.format(category))
         print(50 * '#')
 
-        subcategories = self.get_subcategories(category)
+        category_data_path = os.path.join(self.data_path, category)
+        if not os.path.exists(category_data_path):
+            os.makedirs(category_data_path)
 
-        for subcategory, subcategory_link in subcategories.items():
+        for color in self.colors:
 
-            print(50 * '-')
-            print('Downloading sub-category: {}'.format(subcategory))
+            color_df = pd.DataFrame()
 
-            try:
-                self.download_subcategory(category, subcategory, subcategory_link)
-            except Exception as e:
-                print('Problem with download of subcategory: {}'.format(subcategory), e)
+            max_page = self.get_number_of_pages(category, self.colors[color])
+            print('Color {}: {} pages'.format(color, max_page))
 
-        # save csv to category file (only select rows with category, since df is accumulating all data)
-        csv_file = os.path.join(self.data_path, category + '.csv')
-        print('Writing category data to: {}'.format(csv_file))
-        df_category = self.df_product_info[self.df_product_info.category == category]
-        self.append_csv_file(csv_file, df_category)
+            for page in range(1, max_page+1):
+                print('Downloading page: ', page)
 
-    def download_subcategory(self, category, subcategory, subcategory_link):
-        """
-        For all colors, download the subcategory's products, save the images and store the data in the dataframe.
-        :param category: category
-        :param subcategory: subcategory of the category
-        :param subcategory_link: url for the subcategory webpage
-        :return:
-        """
-
-        # where to save the subcategory images
-        subcategory_data_path = os.path.join(os.path.join(self.data_path, category), subcategory)
-        if os.path.exists(subcategory_data_path):
-            print('Subcategory already downloaded')
-            return
-        else:
-            os.makedirs(subcategory_data_path)
-
-        # download all colors
-        for color_name, color_code in self.colors.items():
-
-            subcategory_color_link = subcategory_link + '?bi_color={}'.format(color_code)
-
-            # get the products list from the subcategory page
-            products = self.download_subcategory_products(subcategory_color_link)
-            print('Color {}: {} products'.format(color_name, len(products)))
-
-            # get information for each product
-            for prod_idx, product in enumerate(products):
                 try:
-                    name, img_id, img_link, details = self.get_product_info(product)
-
-                    # save product image
-                    img_filepath = os.path.join(subcategory_data_path, img_id + self.image_format)
-                    self.save_product_image(img_link, img_filepath, width=400)
-
-                    # save product to dataframe
-                    product_dict = {'img_path': img_filepath,
-                                    'img_url': img_link,
-                                    'category': category,
-                                    'subcategory': subcategory,
-                                    'color': color_name,
-                                    'product_name': name,
-                                    'attributes': ','.join(details)}
-
-                    self.df_product_info = self.df_product_info.append(product_dict, ignore_index=True)
+                    page_df = self.download_page(category, color, page)
+                    color_df.append(page_df)
                 except Exception as e:
-                    print('Problem with downloading product: ', e)
+                    print('Download of page #{} failed'.format(page))
 
-                # sleeep 2 seconds after each product
-                time.sleep(2)
-                self.print_progress_bar(prod_idx + 1, len(products), length=50)
 
-            # save csv to category file (only select rows with category, since df is accumulating all data)
-            csv_file = os.path.join(os.path.join(self.data_path, category), subcategory + '.csv')
-            df_subcategory = self.df_product_info[self.df_product_info.subcategory == subcategory]
-            self.append_csv_file(csv_file, df_subcategory)
+    def get_number_of_pages(self, category, color_code):
+
+        category_color_link = '{}/{}?sort=new&bi_color={}'.format(self.URL_CLOTHES, category, color_code)
+
+        response = self.get_response(category_color_link)
+        category_soup = BeautifulSoup(response.content, 'html.parser')
+
+        try:
+            pagination = category_soup.find('div', class_='paginationWrapper_1wdi4uz')
+            max_page = int(pagination.find_all('li', class_='pageNumbers_ffrt32')[-1].text)
+        except:
+            max_page = 1
+
+        return max_page
+
+    def download_page(self, category, color, page):
+
+        page_link = '{}/{}?sort=new&bi_color={}&page={}'.format(self.URL_CLOTHES, category, self.colors[color], page)
+        page_df = pd.DataFrame()
+
+        # get the products list from the page
+        products = self.download_products(page_link)
+
+        # get information for each product
+        for prod_idx, product in enumerate(products):
+            try:
+                name, img_id, img_link, details = self.get_product_info(product)
+
+                # save product image
+                img_path = os.path.join(category, img_id + self.image_format)
+                img_filepath = os.path.join(self.data_path, img_path)
+                self.save_product_image(img_link, img_filepath, width=400)
+
+                # save product to dataframe
+                product_dict = {'img_path': img_path,
+                                'img_url': img_link,
+                                'category': category,
+                                'color': color,
+                                'product_name': name,
+                                'attributes': ','.join(details)}
+
+                self.append_csv_file(csv_file=self.data_csv, df=product_dict)
+            except Exception as e:
+                print('Problem with downloading product: ', e)
+
+            # sleeep 2 seconds after each product
+            time.sleep(2)
+
+        return page_df
 
     def get_product_info(self, product):
         """
@@ -210,22 +203,22 @@ class AboutYouScraper():
         else:
             print('Image file already exists: ', img_filepath)
 
-    def download_subcategory_products(self, subcategory_link):
+    def download_products(self, url):
         """
-        Download all the product html from the subcategory page
-        :param subcategory_link: URL to the subcategory website
+        Download all the product html from the category page
+        :param url: URL to the category website
         :return: HTML for all the products on the website
         """
 
         # start driver to click on Produktansicht button
         driver = webdriver.Chrome(self.chromedriver_path)
-        driver.get(subcategory_link)
+        driver.get(url)
 
         products = []
 
         # for categories that don't have any products for the given filter, chrome opens a shortened url without
         # the filter, therefore need to check if it is the correct one
-        if driver.current_url == subcategory_link:
+        if driver.current_url == url:
             driver.find_element_by_class_name('button_6u2hqh').click()
 
             # download Produktansicht page and close driver
@@ -241,41 +234,6 @@ class AboutYouScraper():
 
         return products
 
-    def get_subcategories(self, category):
-        """
-        For the given category, get all the names and URLs of all the subcategories in the left menu.
-        :param category: Category for which to get subcategories
-        :return: dictionary with subcategory names and URLs
-        """
-
-        category_url_name = category
-        if ' ' in category_url_name:
-            category_url_name = category_url_name.replace(' ', '')
-        if '&' in category_url_name:
-            category_url_name = category_url_name.replace('&','-und-')
-        if 'ö' in category_url_name:
-            category_url_name = category_url_name.replace('ö', 'oe')
-
-        url_category = self.URL_CLOTHES + '/' + category_url_name
-        response = self.get_response(url_category)
-
-        category_soup = BeautifulSoup(response.content, 'html.parser')
-
-        # find sub-categories of category
-        category_list = category_soup.find('li', title=category.title()).parent
-        sub_categories = {}
-        for sub_category in category_list.find_all('a'):
-            name = sub_category.text.lower()
-            link = self.URL + sub_category['href']
-            sub_categories[name] = link
-
-        # if no subcategories exists, take the category as subcategory
-        if len(sub_categories) == 0:
-            sub_categories[category] = url_category
-
-        print('Found {} subcategories for {}'.format(len(sub_categories), category))
-        return sub_categories
-
     def append_csv_file(self, csv_file, df):
         """
         Write csv file with the scraped data. If such CSV file already exists, append the existing one with new data
@@ -289,7 +247,7 @@ class AboutYouScraper():
             if os.path.exists(csv_file):
                 df_to_save = pd.read_csv(csv_file, sep=';', encoding='utf-8')
 
-            df_to_save = df_to_save.append(df)
+            df_to_save = df_to_save.append(df, ignore_index=True)
             df_to_save = df_to_save.drop_duplicates()
             df_to_save.to_csv(csv_file, index=False, sep=';', encoding='utf-8')
         except Exception as e:
@@ -321,7 +279,7 @@ class AboutYouScraper():
 
 
 def main():
-    scraper = AboutYouScraper(categories=['jumpsuits & overalls'], data_path='/Users/sonynka/HTW/IC/data/aboutyou')
+    scraper = AboutYouScraper(data_path='/Users/sonynka/HTW/IC/data/aboutyou_paging')
     scraper.download_data()
 
 
